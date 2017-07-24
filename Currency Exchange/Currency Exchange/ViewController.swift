bc9c0b92db794abea02cb66e1990669309b9152b
Adrian-Yao <adrian.c.yao@gmail.com>
2017-07-24 09:49:54 -0400

    Currency Exchange

---
+++ Currency Exchange/Currency Exchange/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Currency Exchange
+//
+//  Created by adyao20 on 7/5/17.
+//  Copyright © 2017 adyao20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+    @IBAction func Convert(_ sender: Any) {
+    var convertTo = dollarAmount * 0.88
+}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
